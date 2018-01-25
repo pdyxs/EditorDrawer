5d918d402106c9113ed226d34cf8bd72f2ffb9a7
Paul Sztajer <paul@seethroughstudios.com>
2018-01-25 18:44:25 +0800

    More universal handles

--- Editor/HandlesDrawer.cs
+++ Editor/HandlesDrawer.cs
@@ -7,21 +7,21 @@ public static class HandlesDrawer {
 
     public static Vector2 DoPointHandle(SceneView sceneView, Vector2 cp)
     {
-        return DoPositionHandle(sceneView, cp, 0.15f, Handles.CircleHandleCap);
+        return DoPositionHandle(sceneView, cp, 15f, Handles.CircleHandleCap);
     }
 
     public static Vector2 DoControlHandle(SceneView sceneView, Vector2 cp)
     {
-        return DoPositionHandle(sceneView, cp, 0.075f, Handles.RectangleHandleCap);
+        return DoPositionHandle(sceneView, cp, 7.5f, Handles.RectangleHandleCap);
     }
 
     public static Vector2 DoPositionHandle(SceneView sceneView, Vector2 cp, float size, Handles.CapFunction capFunction)
     {
-        var centrePos = sceneView.camera.WorldToScreenPoint(Vector3.zero);
-        var worldPoint = sceneView.camera.ScreenToWorldPoint(centrePos + cp.to3D());
+        //var centrePos = sceneView.camera.WorldToScreenPoint(Vector3.zero);
+        var worldPoint = cp.to3D();// sceneView.camera.ScreenToWorldPoint(centrePos + cp.to3D());
         var ret = Handles.Slider2D(worldPoint,
                          Vector3.forward, Vector3.up, Vector3.right,
-                         HandleUtility.GetHandleSize(Vector3.zero) * size, capFunction, Vector2.zero);
-        return sceneView.camera.WorldToScreenPoint(ret) - centrePos;
+                         /*HandleUtility.GetHandleSize(Vector3.zero) */ size, capFunction, Vector2.zero);
+        return ret.to2D();
     }
 }
